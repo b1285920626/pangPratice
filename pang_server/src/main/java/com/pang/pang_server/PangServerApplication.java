8e5c24d4886f5a1a495f827291b6c9778c6f83fe
pang <12859206262@qq.com>
2019-11-18 21:06:16 +0800

    初始化

---
+++ pang_server/src/main/java/com/pang/pang_server/PangServerApplication.java
@@ -0,0 +1,13 @@
+package com.pang.pang_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PangServerApplication.class, args);
+    }
+
+}
---
+++ pang_server/src/test/java/com/pang/pang_server/PangServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pang.pang_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
